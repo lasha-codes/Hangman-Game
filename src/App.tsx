8b8aeed99acd0cc25fa375194b3aa676a89c349f
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-14 04:31:10 -0700

    full redux setup is complete and getting an random word on a page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react'
 import Header from './components/Header'
 import { countries } from './countries'
+import { useDispatch } from 'react-redux'
+import { getRandomWord } from './countrySlice'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const randomIndex = Math.floor(Math.random() * countries.length)
+
+  useEffect(() => {
+    dispatch(getRandomWord(countries[randomIndex]))
+  }, [])
+
   return (
     <main className='p-12 relative'>
       <div className='bg-div' />
--- src/countrySlice.ts
+++ src/countrySlice.ts
@@ -2,12 +2,20 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   selectedIdx: null,
+  selectedCountry: [],
 }
 
 const countrySlice = createSlice({
   name: 'countries',
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    getRandomWord: (state, { payload }) => {
+      state.selectedCountry = payload.split('')
+      console.log(state.selectedCountry)
+    },
+  },
 })
 
 export default countrySlice.reducer
+
+export const { getRandomWord } = countrySlice.actions
